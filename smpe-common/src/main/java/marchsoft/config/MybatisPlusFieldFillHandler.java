24e8a6d9f3d26aeae61b50851f6d0fea317daa11
koala0000 <1487660836@qq.com>
2020-11-28 11:29:54 +0800

    fix:查询用户分页处理 feat:mp自动注入

---
+++ smpe-common/src/main/java/marchsoft/config/MybatisPlusFieldFillHandler.java
@@ -0,0 +1,48 @@
+package marchsoft.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import marchsoft.utils.SecurityUtils;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * description:mybatis plus 自定义自动填充策略
+ *
+ * @author RenShiWei
+ * Date: 2020/7/13 16:13
+ **/
+@Component
+@Slf4j
+public class MybatisPlusFieldFillHandler implements MetaObjectHandler {
+
+    /**
+     * description:新增数据，填充创建时间和更新时间
+     *
+     * @author RenShiWei
+     * Date: 2020/7/13 16:14
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("Fill field createAT and updateAT by insert...");
+        this.strictInsertFill(metaObject, "createdAt", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updatedAt", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "createBy", String.class, SecurityUtils.getCurrentUsername());
+        this.strictInsertFill(metaObject, "updateBy", String.class, SecurityUtils.getCurrentUsername());
+    }
+
+    /**
+     * description:更新数据，填充更新时间
+     *
+     * @author RenShiWei
+     * Date: 2020/7/13 16:14
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("Fill field updateAT by update...");
+        this.strictUpdateFill(metaObject, "updatedAt", LocalDateTime.class, LocalDateTime.now());
+        this.strictUpdateFill(metaObject, "updateBy", String.class, SecurityUtils.getCurrentUsername());
+    }
+}
