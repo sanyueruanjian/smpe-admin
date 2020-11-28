649cb711b32f1556721ec09220946a17da4f71f2
unknown <2455619168@qq.com>
2020-11-28 16:57:13 +0800

    fix: fix the sql *

--- smpe-system/src/main/java/marchsoft/modules/system/service/impl/DeptServiceImpl.java
+++ smpe-system/src/main/java/marchsoft/modules/system/service/impl/DeptServiceImpl.java
@@ -429,7 +429,7 @@ public class DeptServiceImpl extends ServiceImpl<DeptMapper, Dept> implements ID
     private LambdaQueryWrapper<Dept> analysisQueryCriteria(DeptQueryCriteria criteria) {
         LambdaQueryWrapper<Dept> wrapper = new LambdaQueryWrapper<>();
         // 查询父部门为pid
-        wrapper.eq(Dept::getPid, criteria.getPid());
+        wrapper.eq(Dept::getPid, criteria.getPid()==null?0:criteria.getPid());
         if (! StrUtil.isBlank(criteria.getName())) {
             // 默认使用Like匹配
             wrapper.like(Dept::getName, criteria.getName());
