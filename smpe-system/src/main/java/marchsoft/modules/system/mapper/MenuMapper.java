226c75ae0e086ee6cbf27a04264e24570dfcb284
qianjin <1542890862@qq.com>
2020-12-01 16:10:46 +0800

    fix:修改添加拦截器后角色修改bug

--- smpe-system/src/main/java/marchsoft/modules/system/mapper/MenuMapper.java
+++ smpe-system/src/main/java/marchsoft/modules/system/mapper/MenuMapper.java
@@ -92,13 +92,9 @@ public interface MenuMapper extends BaseMapper<Menu> {
     @Result(column = "menu_id", property = "id")
     Menu getMenuById(Long menuId);
 
-    /**
-     * 功能描述：
-     *
-     * @param
-     * @return
-     * @author Jiaoqianjin
-     * Date: 2020/11/28 17:19
-     */
-    Integer deleteByMenuIds(Set<Long> menuIds);
+    @Select("SELECT menu_id,pid,sub_count,type,title,name,component,menu_sort,icon,path,i_frame," +
+            "cache,hidden,permission,create_by,update_by,create_time,update_time " +
+            "FROM sys_menu ${ew.customSqlSegment}")
+    @Result(column = "menu_id", property = "id")
+    List<Menu> queryAll(@Param(Constants.WRAPPER)  LambdaQueryWrapper<Menu> menuDtoQueryWrapper);
 }
