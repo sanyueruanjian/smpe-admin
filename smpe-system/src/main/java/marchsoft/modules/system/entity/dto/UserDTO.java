4a6eb0910ba363b74662ff1c7f895f382657e6bb
koala0000 <1487660836@qq.com>
2020-12-22 17:33:54 +0800

    fix:userDto sex

--- smpe-system/src/main/java/marchsoft/modules/system/entity/dto/UserDTO.java
+++ smpe-system/src/main/java/marchsoft/modules/system/entity/dto/UserDTO.java
@@ -39,7 +39,7 @@ public class UserDTO extends BaseDTO implements Serializable {
 
     private String phone;
 
-    private String gender;
+    private Boolean gender;
 
     private String avatarName;
 
