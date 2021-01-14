b749f8195a2c5befabc2dc984a6a77b620c134b5
trudge981219 <1269743128@qq.com>
2021-01-14 21:53:31 +0800

    add logQuery

---
+++ smpe-log/src/main/java/marchsoft/entity/dto/SysLogDTO.java
@@ -0,0 +1,35 @@
+package marchsoft.entity.dto;
+
+import lombok.Data;
+import marchsoft.base.BaseDTO;
+
+import java.io.Serializable;
+
+/**
+ * @author ZhangYuKun
+ * @date 2021/1/14
+ */
+@Data
+public class SysLogDTO extends BaseDTO implements Serializable {
+
+    private Long userId;
+
+    private String description;
+
+    private Integer logType;
+
+    private String method;
+
+    private String params;
+
+    private Long requestTime;
+
+    private String requestIp;
+
+    private String address;
+
+    private String browser;
+
+    private String exceptionDetail;
+
+}
