2ea1ac60480cce67a7fe936512d7adfc41008650
lixiangxiang <204827740@qq.com>
2021-01-25 20:00:37 +0800

    fix:mybatis缓存问题

--- smpe-system/src/main/java/marchsoft/modules/quartz/mapper/QuartzJobMapper.java
+++ smpe-system/src/main/java/marchsoft/modules/quartz/mapper/QuartzJobMapper.java
@@ -4,13 +4,14 @@ import marchsoft.base.BasicMapper;
 import marchsoft.config.mybatisplus.MybatisRedisCache;
 import marchsoft.modules.quartz.entity.QuartzJob;
 import org.apache.ibatis.annotations.CacheNamespace;
+import org.springframework.cache.annotation.CacheConfig;
 
 /**
  * @author lixiangxiang
  * @description /
  * @date 2021/1/14 17:07
  */
-@CacheNamespace(implementation = MybatisRedisCache.class, eviction = MybatisRedisCache.class)
+@CacheConfig(cacheNames = "quartzJob")
 public interface QuartzJobMapper extends BasicMapper<QuartzJob> {
 
 }
--- smpe-system/src/main/java/marchsoft/modules/quartz/mapper/QuartzLogMapper.java
+++ smpe-system/src/main/java/marchsoft/modules/quartz/mapper/QuartzLogMapper.java
@@ -2,12 +2,14 @@ package marchsoft.modules.quartz.mapper;
 
 import marchsoft.base.BasicMapper;
 import marchsoft.modules.quartz.entity.QuartzLog;
+import org.springframework.cache.annotation.CacheConfig;
 
 /**
  * @author lixiangxiang
  * @description
  * @date 2021/1/15 11:37
  */
+@CacheConfig(cacheNames = "quartzLog")
 public interface QuartzLogMapper extends BasicMapper<QuartzLog> {
 
 }
