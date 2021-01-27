8fcf092d4d2ad390016f0323eb4f1c4a011784b8
trudge981219 <1269743128@qq.com>
2021-01-27 14:14:57 +0800

    2021.1.27:1.0 fix conflict

--- smpe-log/src/main/java/marchsoft/controller/SysLogController.java
+++ smpe-log/src/main/java/marchsoft/controller/SysLogController.java
@@ -48,7 +48,7 @@ public class SysLogController {
     @GetMapping(value = "/download")
     @PreAuthorize("@smpe.check()")
     public void downLoad(HttpServletResponse response, SysLogQueryCriteria criteria) throws Exception {
-        log.info(StrUtil.format("【导出日志数据 /api/logs/download】操作人userId：{}，日志查询条件criteria：{}", SecurityUtils.getCurrentUserId(), criteria));
+        log.info(StrUtil.format("【导出日志数据 /api/logs/download】操作人userId：{}，日志查询条件criteria：{}"), SecurityUtils.getCurrentUserId(), criteria);
         criteria.setLogType(Convert.toStr(SysLogEnum.INFO.getLogType()));
         logService.download(logService.queryAll(criteria), response);
     }
@@ -62,7 +62,7 @@ public class SysLogController {
     @GetMapping(value = "/error/download")
     @PreAuthorize("@smpe.check()")
     public void downloadErrorLog(HttpServletResponse response, SysLogQueryCriteria criteria) throws Exception {
-        log.info(StrUtil.format("【导出错误日志数据 /api/logs/error/download】操作人userId：{}，日志查询条件criteria：{}", SecurityUtils.getCurrentUserId(), criteria));
+        log.info(StrUtil.format("【导出错误日志数据 /api/logs/error/download】操作人userId：{}，日志查询条件criteria：{}"), SecurityUtils.getCurrentUserId(), criteria);
         criteria.setLogType(Convert.toStr(SysLogEnum.ERROR.getLogType()));
         logService.download(logService.queryAll(criteria), response);
     }
@@ -75,7 +75,7 @@ public class SysLogController {
     @GetMapping
     @PreAuthorize("@smpe.check()")
     public Result<IPage<SysLogDTO>> query(SysLogQueryCriteria criteria, PageVO pageVO) {
-        log.info(StrUtil.format("【查询日志数据 /api/logs】操作人userId：{}，日志查询条件criteria：{}，分页pageVo：{}", SecurityUtils.getCurrentUserId(), criteria, pageVO));
+        log.info(StrUtil.format("【查询日志数据 /api/logs】操作人userId：{}，日志查询条件criteria：{}，分页pageVo：{}"), SecurityUtils.getCurrentUserId(), criteria, pageVO);
         criteria.setLogType(Convert.toStr(SysLogEnum.INFO.getLogType()));
         return Result.success(logService.queryAll(criteria, pageVO.buildPage()));
     }
@@ -88,7 +88,7 @@ public class SysLogController {
     @GetMapping(value = "/error")
     @PreAuthorize("@smpe.check()")
     public Result<IPage<SysLogDTO>> queryErrorLog(SysLogQueryCriteria criteria, PageVO pageVO) {
-        log.info(StrUtil.format("【查询日志数据 /api/logs/error】操作人userId：{}，日志查询条件criteria：{}，分页pageVo：{}", SecurityUtils.getCurrentUserId(), criteria, pageVO));
+        log.info(StrUtil.format("【查询日志数据 /api/logs/error】操作人userId：{}，日志查询条件criteria：{}，分页pageVo：{}"), SecurityUtils.getCurrentUserId(), criteria, pageVO);
         criteria.setLogType(Convert.toStr(SysLogEnum.ERROR.getLogType()));
         return Result.success(logService.queryAll(criteria, pageVO.buildPage()));
     }
@@ -100,7 +100,7 @@ public class SysLogController {
     @GetMapping("/error/{id}")
     @PreAuthorize("@smpe.check()")
     public Result<SysLogDTO> queryErrorLogs(@PathVariable Long id) {
-        log.info(StrUtil.format("【查询日志数据 /api/logs/error/{id}】操作人userId：{}", SecurityUtils.getCurrentUserId()));
+        log.info(StrUtil.format("【查询日志数据 /api/logs/error/{id}】操作人userId：{}"), SecurityUtils.getCurrentUserId());
         return Result.success(logService.findByErrDetail(id));
     }
 
@@ -109,7 +109,7 @@ public class SysLogController {
     @DeleteMapping(value = "/del/error")
     @PreAuthorize("@smpe.check()")
     public Result<Object> delAllErrorLog() {
-        log.info(StrUtil.format("【查询日志数据 /api/logs/del/error】操作人userId：{}", SecurityUtils.getCurrentUserId()));
+        log.info(StrUtil.format("【查询日志数据 /api/logs/del/error】操作人userId：{}"), SecurityUtils.getCurrentUserId());
         logService.delAllByError();
         return Result.success();
     }
@@ -119,7 +119,7 @@ public class SysLogController {
     @DeleteMapping(value = "/del/info")
     @PreAuthorize("@smpe.check()")
     public Result<Object> delAllInfoLog() {
-        log.info(StrUtil.format("【查询日志数据 /api/logs/del/info】操作人userId：{}", SecurityUtils.getCurrentUserId()));
+        log.info(StrUtil.format("【查询日志数据 /api/logs/del/info】操作人userId：{}"), SecurityUtils.getCurrentUserId());
         logService.delAllByInfo();
         return Result.success();
     }
